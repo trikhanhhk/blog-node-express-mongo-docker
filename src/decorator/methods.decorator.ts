7c35fdeff68da3037405f10ebe617d63f55acda5
khanhnt <nguyentrikhanh123hk@gmail.com>
2024-06-04 22:20:04 +0700

    create decorator for api

---
+++ src/decorator/methods.decorator.ts
@@ -0,0 +1,30 @@
+import { Methods } from '~/constants/enum'
+import { IRouter } from '~/interface/router.interface'
+import { MetadataKeys } from './meta.keys'
+
+const methodDecoratorFactory = (method: Methods) => {
+  return (path: string): any => {
+    return (target: object, propertykey: string | symbol, descriptor: TypedPropertyDescriptor<any>) => {
+      const controllerClass = target.constructor
+
+      const routers: IRouter[] = Reflect.hasMetadata(MetadataKeys.ROUTERS, controllerClass)
+        ? Reflect.getMetadata(MetadataKeys.ROUTERS, controllerClass)
+        : []
+
+      routers.push({
+        method,
+        path,
+        handlerName: propertykey
+      })
+
+      Reflect.defineMetadata(MetadataKeys.ROUTERS, routers, controllerClass)
+      // return descriptor
+    }
+  }
+}
+
+export const Get = methodDecoratorFactory(Methods.GET)
+export const Post = methodDecoratorFactory(Methods.POST)
+export const Put = methodDecoratorFactory(Methods.PUT)
+export const Patch = methodDecoratorFactory(Methods.PATCH)
+export const Delete = methodDecoratorFactory(Methods.DELETE)
