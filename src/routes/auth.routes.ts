0b15d8ec1d8d92aab01f2220dda819828fcce3f5
khanhnt <nguyentrikhanh123hk@gmail.com>
2024-06-04 22:25:11 +0700

    delete routes

--- src/routes/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { signin } from "../controllers/auth.controller";
-
-const router = Router()
-
-router.post('/auth/signin', signin)
-
-export default router
